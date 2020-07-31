e61194d94ec0c4f3fc9e0b6d955ed17a92e3957f
Quazaru <qilaur@mail.ru>
2020-07-31 13:30:44 +0300

    init: start songbird task

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+import 'normalize.css';
+import '../assets/css/styles.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './components/App';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
